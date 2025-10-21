69e9366980826d003c9ca5970fc99e5a4cbf95df
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-10-21 06:37:18 +0300

    Implement code changes to enhance functionality and improve performance

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,12 @@
  
- 
+ import image from '../image.png';
  function Header() {
     return (
     <>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li style={{ color: 'blue', fontWeight: 'bold', fontSize: '100px' }}>Home</li>
+          <li >About</li>
+          <li >Contact</li>
         </ul>
       </>
     );
