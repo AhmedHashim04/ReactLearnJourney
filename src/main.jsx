ec71a1bcc2a51ab44fb306c5458f077f788692bd
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-10-21 05:35:06 +0300

    feat: add initial CSS styling for list items and clean up code formatting

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './Header.jsx'
 import App from './App.jsx'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -9,6 +10,6 @@ createRoot(document.getElementById('root')).render(
     <Header />
     <App />
 
-    
+
   </StrictMode>,
 )
