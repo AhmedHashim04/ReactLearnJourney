01dfc6ecf829efb448762525197954981209641d
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-10-21 04:39:28 +0300

    feat: update README and HTML structure, enhance CSS styling, and modify App component

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+
+# NEW
+npm create vite@latest my-vite-app
+cd my-vite-app
+npm install
 npm run dev
+
+
+# OLD
+
+npx ceate-react-app my-app
+cd my-app
+npm start
--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,9 @@ function App() {
 
   return (
     <>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Its Ahmed Hashim
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -28,6 +24,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      
     </>
   )
 }
