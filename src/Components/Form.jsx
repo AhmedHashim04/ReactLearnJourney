cc0d5746833637c9feb408d6baf37dc5a356f085
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-10-25 00:09:38 +0300

    feat: implement controlled inputs in Form component for first name, last name, and email

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
 
 export default function Form() {
+    const [firstName, setF] = useState("");
+    const [lastName, setL] = useState("");
+    const [email, setE] = useState("");
+
+    console.log(firstName)
+
     return(
             <div className="FormSignup">
                 <form className="signupForm" action="" method="post">
                     <label htmlFor="1"> First Name</label>
-                    <input id="1" type="text" placeholder="First Name" required />
+                    <input value={firstName} id="1" type="text" placeholder="First Name" required 
+                           onChange={(e) => setF(e.target.value)} />
+
                     <label htmlFor="2"> Last Name</label>
-                    <input id="2" type="text" placeholder="Last Name" required />
+                    <input value={lastName} id="2" type="text" placeholder="Last Name" required 
+                           onChange={(e) => setL(e.target.value)} />
+
                     <label htmlFor="3"> Email</label>
-                    <input id="3" type="email" placeholder="First Name" required />
+                    <input value={email} id="3" type="email" placeholder="Email" required 
+                           onChange={(e) => setE(e.target.value)} />
+
                     <button type="submit">Sign Up</button>
 
                 </form>
